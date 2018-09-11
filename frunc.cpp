601be880f85a13759ef340ac1c9dd34d3ad03170
samiyal <777samiyal777@gmail.com>
2018-09-11 22:03:05 +0530

    frunc.cpp

---
+++ frunc.cpp
@@ -0,0 +1,30 @@
+/* 
+objective:-  the aim of this program to make a function of argument void and whgich printds the dfined values of thew variables 
+
+author:-samiyal massy
+
+
+
+   */
+
+
+#include<iostream.h>
+#include<conio.h>
+void fun1(void)
+{
+  int x;
+x=0;
+
+}
+void func2(void)
+{
+  int x;
+x=-199;
+
+}
+void main()
+{
+func1();
+func2();
+getch();
+}
